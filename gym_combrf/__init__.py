af12a652000e4d8085d61905cc5b018b833fb72a
psusarla95 <praneeth.susarla@oulu.fi>
2020-09-08 10:58:40 +0300

    initialize v2-10 Rf environments

--- gym_combrf/__init__.py
+++ gym_combrf/__init__.py
@@ -9,3 +9,53 @@ register(
     id='combrf-v2',
     entry_point='gym_combrf.envs:CombRF_Env_v2',
 )
+
+register(
+    id='combrfinit-v2',
+    entry_point='gym_combrf.envs:CombRF_Env_v2initresult',
+)
+
+register(
+    id='combrf-v3',
+    entry_point='gym_combrf.envs:CombRF_Env_v3',
+)
+
+register(
+    id='combrf-v4',
+    entry_point='gym_combrf.envs:CombRF_Env_v4',
+)
+
+register(
+    id='combrf-v5',
+    entry_point='gym_combrf.envs:CombRF_Env_cab',
+)
+
+register(
+    id='combrf-v6',
+    entry_point='gym_combrf.envs:CombRF_Env_v6',
+)
+
+register(
+    id='combrf-v7',
+    entry_point='gym_combrf.envs:CombRF_Env_v7',
+)
+
+register(
+    id='combrf-v8',
+    entry_point='gym_combrf.envs:CombRF_Env_v8',
+)
+
+register(
+    id='combrf-v9',
+    entry_point='gym_combrf.envs:CombRF_Env_v9',
+)
+
+register(
+    id='combrf-v10',
+    entry_point='gym_combrf.envs:CombRF_Env_v10',
+)
+
+register(
+    id='combrf-v11',
+    entry_point='gym_combrf.envs:CombRF_Env_v7_270820',
+)
